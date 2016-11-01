a4bf71535bf6c3c4f80da26627f56b6331055b91
allanjie <allanmcgrady@gmail.com>
2016-11-01 12:49:58 +0800

    Current demo parser

--- src/main/java/org/statnlp/allan/depner/demo/DependencyParserDemo.java
+++ src/main/java/org/statnlp/allan/depner/demo/DependencyParserDemo.java
@@ -23,37 +23,51 @@ public class DependencyParserDemo  {
 
   /** A logger for this class */
   private static Redwood.RedwoodChannels log = Redwood.channels(DependencyParserDemo.class);
-  public static void main(String[] args) {
-    String modelPath = DependencyParser.DEFAULT_MODEL;
-    String taggerPath = "edu/stanford/nlp/models/pos-tagger/english-left3words/english-left3words-distsim.tagger";
+  
+  public static void demoTestWithModel(String[] args){
+	  String modelPath = DependencyParser.DEFAULT_MODEL;
+	    String taggerPath = "edu/stanford/nlp/models/pos-tagger/english-left3words/english-left3words-distsim.tagger";
 
-    for (int argIndex = 0; argIndex < args.length; ) {
-      switch (args[argIndex]) {
-        case "-tagger":
-          taggerPath = args[argIndex + 1];
-          argIndex += 2;
-          break;
-        case "-model":
-          modelPath = args[argIndex + 1];
-          argIndex += 2;
-          break;
-        default:
-          throw new RuntimeException("Unknown argument " + args[argIndex]);
-      }
-    }
+	    for (int argIndex = 0; argIndex < args.length; ) {
+	      switch (args[argIndex]) {
+	        case "-tagger":
+	          taggerPath = args[argIndex + 1];
+	          argIndex += 2;
+	          break;
+	        case "-model":
+	          modelPath = args[argIndex + 1];
+	          argIndex += 2;
+	          break;
+	        default:
+	          throw new RuntimeException("Unknown argument " + args[argIndex]);
+	      }
+	    }
 
-    String text = "I can almost always tell when movies use fake dinosaurs.";
+	    String text = "I can almost always tell when movies use fake dinosaurs.";
 
-    MaxentTagger tagger = new MaxentTagger(taggerPath);
-    DependencyParser parser = DependencyParser.loadFromModelFile(modelPath);
+	    MaxentTagger tagger = new MaxentTagger(taggerPath);
+	    DependencyParser parser = DependencyParser.loadFromModelFile(modelPath);
 
-    DocumentPreprocessor tokenizer = new DocumentPreprocessor(new StringReader(text));
-    for (List<HasWord> sentence : tokenizer) {
-      List<TaggedWord> tagged = tagger.tagSentence(sentence);
-      GrammaticalStructure gs = parser.predict(tagged);
+	    DocumentPreprocessor tokenizer = new DocumentPreprocessor(new StringReader(text));
+	    for (List<HasWord> sentence : tokenizer) {
+	      List<TaggedWord> tagged = tagger.tagSentence(sentence);
+	      GrammaticalStructure gs = parser.predict(tagged);
 
-      // Print typed dependencies
-      log.info(gs);
-    }
+	      // Print typed dependencies
+	      log.info(gs);
+	    }
   }
+  
+  public static void demoTrainWithModel(){
+	  
+  }
+  
+  public static void main(String[] args) {
+    
+  }
+
+
+  
+  
+  
 }
