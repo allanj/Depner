560b2e7287ef1614dffbdac9e240be37dfa48381
allanjie <allanmcgrady@gmail.com>
2016-11-04 14:14:30 +0800

    Fix a bug in getOracle operation

--- src/main/java/org/statnlp/allan/nner/NEStandard.java
+++ src/main/java/org/statnlp/allan/nner/NEStandard.java
@@ -5,66 +5,65 @@ import java.util.List;
 
 import edu.stanford.nlp.trees.TreebankLanguagePack;
 
-
 /**
- * Define an NE standard transition-based parser for named entity recognition 
- * A very basic transition-based approach for named entity recognition.
+ * Define an NE standard transition-based parser for named entity recognition A
+ * very basic transition-based approach for named entity recognition.
  *
  * @author Allan Jie
  */
 public class NEStandard extends NERParsingSystem {
 
-  public NEStandard(TreebankLanguagePack tlp, List<String> labels, boolean verbose) {
-    super(tlp, labels, verbose);
-  }
+	public NEStandard(TreebankLanguagePack tlp, List<String> labels, boolean verbose) {
+		super(tlp, labels, verbose);
+	}
 
-  
-  @Override
-  public boolean isTerminal(NEConfiguration c) {
-	//means all words are shifted
-    return c.getBufferSize() == 0;
-  }
+	@Override
+	public boolean isTerminal(NEConfiguration c) {
+		// means all words are shifted
+		return c.getBufferSize() == 0;
+	}
 
-  @Override
-  public void makeTransitions() {
-    transitions = new ArrayList<>();
-    //only have shift operation
-    for (String label : labels)
-      transitions.add("S(" + label + ")");
-    
-  }
+	@Override
+	public void makeTransitions() {
+		transitions = new ArrayList<>();
+		// only have shift operation
+		for (String label : labels)
+			transitions.add("S(" + label + ")");
 
-  @Override
-  public NEConfiguration initialConfiguration(Sequence s) {
-    NEConfiguration c = new NEConfiguration(s);
-    int length = s.size();
+	}
 
-    // For each token, add dummy elements to the configuration's tree
-    // and add the words onto the buffer
-    for (int i = 0; i < length; ++i) {
-      //put the token to buffer
-      c.buffer.add(i);
-    }
+	@Override
+	public NEConfiguration initialConfiguration(Sequence s) {
+		NEConfiguration c = new NEConfiguration(s);
+		int length = s.size();
 
-    return c;
-  }
+		// For each token, add dummy elements to the configuration's tree
+		// and add the words onto the buffer
+		for (int i = 0; i < length; ++i) {
+			// put the token to buffer
+			c.buffer.add(i);
+		}
 
-  @Override
-  public boolean canApply(NEConfiguration c, String t) {
-    int nBuffer = c.getBufferSize();
-    return nBuffer > 0;
-  }
+		return c;
+	}
 
-  @Override
-  public void apply(NEConfiguration c, String t) {
-    c.shift(t.substring(2, t.length() - 1));
-  }
+	@Override
+	public boolean canApply(NEConfiguration c, String t) {
+		int nBuffer = c.getBufferSize();
+		return nBuffer > 0;
+	}
 
-  // O(n) implementation
-  @Override
-  public String getOracle(NEConfiguration c, Sequence dner) {
-    String label = dner.get(0)[0];
-    return "S(" + label + ")";
-  }
+	@Override
+	public void apply(NEConfiguration c, String t) {
+		c.shift(t.substring(2, t.length() - 1));
+	}
+
+	// O(n) implementation
+	@Override
+	public String getOracle(NEConfiguration c, Sequence dner) {
+		int b0 = c.getBuffer(0);
+		String label = dner.get(b0)[0];
+		return "S(" + label + ")";
+	}
 
 }
